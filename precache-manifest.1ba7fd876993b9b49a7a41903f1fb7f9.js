b3766954930e53effa7e77679312c9fc99b94732
Pradum Kumar Dubey <87878086+AKASHPRADUM@users.noreply.github.com>
2023-07-26 18:15:30 +0530

    Add files via upload

---
+++ precache-manifest.1ba7fd876993b9b49a7a41903f1fb7f9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4a8ecf26a234731ab18",
+    "url": "/All-Webpages/static/css/main.ee4414ab.chunk.css"
+  },
+  {
+    "revision": "c4a8ecf26a234731ab18",
+    "url": "/All-Webpages/static/js/main.c4a8ecf2.chunk.js"
+  },
+  {
+    "revision": "28e17569edebe29d17e7",
+    "url": "/All-Webpages/static/js/1.28e17569.chunk.js"
+  },
+  {
+    "revision": "e73241764b1ccbaeb52f",
+    "url": "/All-Webpages/static/js/runtime~main.e7324176.js"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/All-Webpages/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/All-Webpages/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/All-Webpages/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/All-Webpages/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/All-Webpages/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/All-Webpages/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "395978468260ee47e69fc07ed9601fe9",
+    "url": "/All-Webpages/static/media/portfolio7.39597846.jpg"
+  },
+  {
+    "revision": "8d243f2661890b5d6b958aa58d123c5b",
+    "url": "/All-Webpages/static/media/portfolio8.8d243f26.jpg"
+  },
+  {
+    "revision": "aa67b581b823e40c29d50b22a673cef9",
+    "url": "/All-Webpages/static/media/portfolio9.aa67b581.png"
+  },
+  {
+    "revision": "afb94dd6e80c1daddaa42dd449c5d21d",
+    "url": "/All-Webpages/static/media/portfolio10.afb94dd6.jpg"
+  },
+  {
+    "revision": "e4565e0b86521294b05f9eebdf61b8e6",
+    "url": "/All-Webpages/static/media/portfolio11.e4565e0b.jpg"
+  },
+  {
+    "revision": "6d32eef4d98e62e758328fdf98514637",
+    "url": "/All-Webpages/index.html"
+  }
+];
